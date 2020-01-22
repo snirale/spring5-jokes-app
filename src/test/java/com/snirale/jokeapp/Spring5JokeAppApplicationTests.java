8bfc87ee22ccb48c6a735710e5a1503669d3f2ac
Shrinivas Nriale <sn.nirale@gmail.com>
2020-01-22 12:48:00 +0530

    Initial commit

---
+++ src/test/java/com/snirale/jokeapp/Spring5JokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snirale.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
